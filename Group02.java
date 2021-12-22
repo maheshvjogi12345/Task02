1659e4053712725d30e4ed1cb99f3bbec0142a34
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 18:04:42 +0530

    num without for loop

---
+++ Group02.java
@@ -0,0 +1,7 @@
+class Group02{
+	public static void main(String arg[]){
+		long []num = {7892721595l,8296796488l,8147214046l,9535658430l,9482736775l,8123019051l,9731008824l};
+		
+		System.out.println(num[0]+"  "+num[1]+"  "+num[2]+"  "+num[3]+"  "+num[4]+"  "+num[5]+"  "+num[6])
+	}
+}
