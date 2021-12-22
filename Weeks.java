b057bf7b13e2cc54339da66d71acbc39bbdb310a
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 21:27:39 +0530

    array exe bound

---
+++ Weeks.java
@@ -0,0 +1,11 @@
+class Weeks{
+	public static void  main(String[] ag){
+		
+		String []days ={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+		for(int ix = 0; ix<=days.length; ix++){
+		
+		System.out.println(days[ix]);
+		}
+	}
+}
+		
