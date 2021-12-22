7b0b4a197d7fa5208ad0089fbb3836e879d2d03f
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 17:59:55 +0530

    Month without for loop

---
+++ Month02.java
@@ -0,0 +1,9 @@
+class Month02{
+	public static void main(String arg[]){
+		String []mon = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+		
+
+		System.out.println(mon[0]+"  "+mon[1]+"  "+mon[2]+"  "+mon[3]+"  "+mon[4]+"  "+mon[5]+"  "+mon[6]+"  "+mon[7]+"  "+mon[8]+"  "+mon[9]+"  "+mon[10]+"  "+mon[11]);
+
+	}
+}
