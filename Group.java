75d58858a07c49fbf8534c7c00ab4c905be0f848
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 18:03:34 +0530

    num with for loop

---
+++ Group.java
@@ -0,0 +1,8 @@
+class Group{
+	public static void main(String arg[]){
+		long []num = {7892721595l,8296796488l,8147214046l,9535658430l,9482736775l,8123019051l,9731008824l};
+		for(int index=0; index<num.length;index++){
+			System.out.println(num[index]);
+			}
+	}
+}
