fcd54fa8ebbfa1f3fc48aae3d95b8872c501fe65
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 17:58:56 +0530

    Month with for loop

---
+++ Month.java
@@ -0,0 +1,8 @@
+class Month{
+	public static void main(String arg[]){
+		String []months = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+		for(int index=0; index<months.length;index++){
+			System.out.println(months[index]);
+			}
+	}
+}
