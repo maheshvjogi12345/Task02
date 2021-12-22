e33384e83af6a091568cc56e6952e6b009d67fa1
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-22 17:56:48 +0530

    weekdays without for loop

---
+++ Weekdays02.java
@@ -0,0 +1,10 @@
+class Weekdays02{
+	public static void  main(String[] ag){
+		
+		String []days ={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+		
+		System.out.println(days[0]+"  "+days[1]+"  "+days[2]+"  "+days[3]+"  "+days[4]+"  "+days[5]+"  "+days[6]);
+			
+	}
+}
+		
